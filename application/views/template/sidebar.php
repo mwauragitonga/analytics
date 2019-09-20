0bcfa338e4bd0ef3af46e4dfaff1d102c6701d34
Cyrus Muchiri <cyrusmuchirichomba@gmail.com>
2019-09-20 16:28:51 +0300

    Unclickable link of web analysis

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -16,7 +16,7 @@
         <!-- sidebar menu: : style can be found in sidebar.less -->
         <ul class="sidebar-menu" data-widget="tree">
             <li class="header">MAIN NAVIGATION</li>
-            <li class="active treeview">
+            <li class="active">
                 <a href="<?php echo base_url(); ?>general">
                     <i class="fa fa-dashboard"></i> <span>General</span>
 
