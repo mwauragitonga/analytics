fbaa932dd18f8ed003e0a893a36e16ce48a7bcb1
mwauragitonga <mwauragitonga12@gmail.com>
2019-10-28 15:39:12 +0300

    added registration source to signups table

--- application/models/Analytics_model.php
+++ application/models/Analytics_model.php
@@ -354,12 +354,13 @@ class Analytics_model extends CI_Model
 
 	}
 	public function signUps_By_Day($date){
-		$this->db->select("fname , mobile, gender ,study_levels.level_name,schools.name as school_name, student_subscriptions.status ,users.user_status as userstatus");
+		$this->db->select("fname , mobile, gender , user_registration_source.source_name  as source ,study_levels.level_name,schools.name as school_name, student_subscriptions.status ,users.user_status as userstatus");
 		$this->db->from("users");
 		$this->db->join("students","users.user_id = students.user_id");
 		$this->db->join("schools","students.school_code = schools.school_code");
 		$this->db->join("study_levels","students.study_level = study_levels.level_code");
 		$this->db->join("student_subscriptions","users.user_id = student_subscriptions.user_id");
+		$this->db->join('user_registration_source', 'users.registration_source = user_registration_source.source_code');
 		$this->db->like("date_joined", $date);
 		$query = $this->db->get()->result();
 		return $query;
