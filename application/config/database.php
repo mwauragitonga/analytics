abe2fdaa9fb9496685b16a614264c1a4b6b4f466
Cyrus-Muchiri <cyrusmuchirichomba@gmail.com>
2019-10-31 17:02:41 +0300

    db

--- application/config/database.php
+++ application/config/database.php
@@ -90,7 +90,7 @@ $db['default'] = array(
 	'swap_pre' => '',
 	'encrypt' => FALSE,
 	'compress' => FALSE,
-	'stricton' => FALSE,
+'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
 );
