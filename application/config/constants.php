48ea282712cb10aabb5f0f4020538e245cfc3d48
Cyrus-Muchiri <cyrusmuchirichomba@gmail.com>
2020-02-19 16:56:39 +0300

    App usage time

--- application/config/constants.php
+++ application/config/constants.php
@@ -84,3 +84,5 @@ defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 define('VIDEOCAP', 3600);
+define('APPUSAGE', 7200);
+
--- application/models/AppModel.php
+++ application/models/AppModel.php
@@ -39,7 +39,7 @@ class AppModel extends CI_Model
 		$this->db->where('(content_type ="App Usage" OR content_type ="App+Usage")');
 		$query = $this->db->get();
 		$appUsageTimes = $query->result();
-		$totalAppUsageTime = $this->calculateTime($appUsageTimes,'hours');
+		$totalAppUsageTime = $this->calculateTime($appUsageTimes, 'hours', 'APPUSAGE');
 
 		return round($totalAppUsageTime, 2);
 	}
@@ -68,22 +68,26 @@ class AppModel extends CI_Model
 		return $query->num_rows();
 	}
 
-	public function signIns($mode='unique')
+	public function signIns($mode = 'unique')
 	{
 		$this->db->select('*');
 		$this->db->from('mobile_analysis_data');
 		$this->db->where('content_type', 'Signin');
-		if($mode=='unique'){$this->db->group_by('user_id');}
+		if ($mode == 'unique') {
+			$this->db->group_by('user_id');
+		}
 		$query = $this->db->get();
 		return $query->num_rows();
 	}
-	public function users_signIns(){
+
+	public function users_signIns()
+	{
 		$this->db->select('users.fname,mobile_analysis_data.start_stamp');
 		$this->db->from('mobile_analysis_data');
-		$this->db->join('users',',mobile_analysis_data.user_id = users.user_id');
+		$this->db->join('users', ',mobile_analysis_data.user_id = users.user_id');
 		$this->db->where('content_type', 'Signin');
 		$this->db->group_by('mobile_analysis_data.user_id');
-		$this->db->order_by('start_stamp','DESC');
+		$this->db->order_by('start_stamp', 'DESC');
 		$query = $this->db->get()->result();
 		return $query;
 	}
@@ -100,7 +104,7 @@ class AppModel extends CI_Model
 		$this->db->group_by('mobile_analysis_data.user_ID');
 		$this->db->where('(content_type ="Videos" OR content_type ="Ebooks")');
 		$this->db->where('(start_stamp < end_stamp)');
-	//	$this->db->limit(10);
+		//	$this->db->limit(10);
 		$this->db->order_by('appMinutes', 'DESC');
 
 		$query = $this->db->get()->result();
@@ -163,9 +167,9 @@ class AppModel extends CI_Model
 				$dat2->avgWatchSecs = VIDEOCAP;
 			}
 		}
-		$data3 = array_merge($data,$data2);
-		usort($data3, function($a, $b) {
-			return $b->avgWatchSecs*$b->count <=> $a->avgWatchSecs*$a->count;
+		$data3 = array_merge($data, $data2);
+		usort($data3, function ($a, $b) {
+			return $b->avgWatchSecs * $b->count <=> $a->avgWatchSecs * $a->count;
 		});
 		return $data3;
 	}
@@ -186,20 +190,22 @@ class AppModel extends CI_Model
 				$dat->avgReadSecs = VIDEOCAP;
 			}
 		}
-		usort($data, function($a, $b) {
-			return $b->avgReadSecs*$b->count <=> $a->avgReadSecs*$a->count;
+		usort($data, function ($a, $b) {
+			return $b->avgReadSecs * $b->count <=> $a->avgReadSecs * $a->count;
 		});
 		//print_r($data);
 		return $data;
 
 	}
-	function userStudyInfo($user_id){
+
+	function userStudyInfo($user_id)
+	{
 		$this->db->select("SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(end_stamp,start_stamp)))) as readSecs,AVG(TIME_TO_SEC(TIMEDIFF(end_stamp,start_stamp))) as avgReadSecs,multimedia_content.file_name,count(index_ID) as count");
 		$this->db->from("mobile_analysis_data");
-		$this->db->join('users',"users.user_id=mobile_analysis_data.user_id");
-		$this->db->join('multimedia_content','subtopic_ID = multimedia_content.file_id');
-		$this->db->where('mobile_analysis_data.content_type',"Ebooks");
-		$this->db->where('mobile_analysis_data.user_id',$user_id);
+		$this->db->join('users', "users.user_id=mobile_analysis_data.user_id");
+		$this->db->join('multimedia_content', 'subtopic_ID = multimedia_content.file_id');
+		$this->db->where('mobile_analysis_data.content_type', "Ebooks");
+		$this->db->where('mobile_analysis_data.user_id', $user_id);
 		$this->db->where('(start_stamp < end_stamp)');
 
 		$this->db->group_by('subtopic_ID');
@@ -214,10 +220,10 @@ class AppModel extends CI_Model
 
 		$this->db->select("name as file_name,SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(end_stamp,start_stamp)))) as watchSecs,AVG(TIME_TO_SEC(TIMEDIFF(end_stamp,start_stamp))) as avgWatchSecs,count(index_ID) as count");
 		$this->db->from("mobile_analysis_data");
-		$this->db->join('users',"users.user_id=mobile_analysis_data.user_id");
-		$this->db->join('subtopics','subtopic_ID = subtopics.subtopicID');
-		$this->db->where('mobile_analysis_data.content_type',"Videos");
-		$this->db->where('mobile_analysis_data.user_id',$user_id);
+		$this->db->join('users', "users.user_id=mobile_analysis_data.user_id");
+		$this->db->join('subtopics', 'subtopic_ID = subtopics.subtopicID');
+		$this->db->where('mobile_analysis_data.content_type', "Videos");
+		$this->db->where('mobile_analysis_data.user_id', $user_id);
 		$this->db->where('(start_stamp < end_stamp)');
 		$this->db->group_by('subtopic_ID');
 		$this->db->order_by('avgWatchSecs');
@@ -230,10 +236,10 @@ class AppModel extends CI_Model
 
 		$this->db->select("file_name,SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(end_stamp,start_stamp)))) as watchSecs,AVG(TIME_TO_SEC(TIMEDIFF(end_stamp,start_stamp))) as avgWatchSecs,count(index_ID) as count");
 		$this->db->from("mobile_analysis_data");
-		$this->db->join('users',"users.user_id=mobile_analysis_data.user_id");
-		$this->db->join('multimedia_content','subtopic_ID = multimedia_content.file_id');
-		$this->db->where('mobile_analysis_data.content_type',"Videos");
-		$this->db->where('mobile_analysis_data.user_id',$user_id);
+		$this->db->join('users', "users.user_id=mobile_analysis_data.user_id");
+		$this->db->join('multimedia_content', 'subtopic_ID = multimedia_content.file_id');
+		$this->db->where('mobile_analysis_data.content_type', "Videos");
+		$this->db->where('mobile_analysis_data.user_id', $user_id);
 		$this->db->where('(start_stamp < end_stamp)');
 		$this->db->group_by('subtopic_ID');
 		$this->db->order_by('watchSecs');
@@ -244,26 +250,29 @@ class AppModel extends CI_Model
 			}
 		}
 
-		$data3 = array_merge($data_videos,$data_videos2);
-		usort($data3, function($a, $b) {
+		$data3 = array_merge($data_videos, $data_videos2);
+		usort($data3, function ($a, $b) {
 			return $b->watchSecs <=> $a->watchSecs;
 		});
 
-		$data= array("ebooks"=>$data_ebooks,"videos" =>$data3);
+		$data = array("ebooks" => $data_ebooks, "videos" => $data3);
 
 		return $data;
 
 	}
-	public function getUserName($user_id){
+
+	public function getUserName($user_id)
+	{
 		$this->db->select('fname');
 		$this->db->from('users');
-		$this->db->where('user_id',$user_id);
-		$fname = $this->db->get()->row() ;
+		$this->db->where('user_id', $user_id);
+		$fname = $this->db->get()->row();
 		return $fname;
 	}
 
-	private function calculateTime($arr_time,$format = 'mins')
+	private function calculateTime($arr_time, $format = 'mins', $usage = '')
 	{
+		$app_time = 0;
 		$count = 0;
 		$time_seconds = array();
 		foreach ($arr_time as $time) {
@@ -273,22 +282,26 @@ class AppModel extends CI_Model
 			if ($timee <= 0) { //checks and skips elements with no end time
 				continue;
 			} else {
-				if($timee > VIDEOCAP){
-					$timee = VIDEOCAP;
+				if ($usage == 'APPUSAGE' && $timee > APPUSAGE) {
+					$timee = APPUSAGE;
+				} elseif ($timee > VIDEOCAP && empty($usage)) {
+						$timee = VIDEOCAP;
+					}
+					$time_seconds[$count] = $timee;
+					$count++;
 				}
-				$time_seconds[$count] = $timee;
-				$count++;
 			}
-		}
 
 		$totalTime = array_sum($time_seconds); // in seconds
 		if ($format == 'mins') {
 			return $totalTime / 60; //in minutes
-		}else if ($format == 'hours'){
+		} else if ($format == 'hours') {
 			return $totalTime / 60 / 60; //in hours
 		}
 	}
-	public function users_unique(){
+
+	public function users_unique()
+	{
 
 		try {
 			$this->db->query("DELETE t1 FROM mobile_analysis_data t1
@@ -299,14 +312,16 @@ WHERE
 	t1.user_ID = t2.user_ID;");
 
 			return true;
-		}catch (Exception $e){
+		} catch (Exception $e) {
 			return false;
 		}
 
 	}
-	function make_unique($user_id = ""){
 
-	//	echo  $user_id;
+	function make_unique($user_id = "")
+	{
+
+		//	echo  $user_id;
 
 	}
 
