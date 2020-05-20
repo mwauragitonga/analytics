4c0fa05677defa55eef198005eb68b4efc9d79f7
stanMandela <smstan8@gmail.com>
2020-05-20 09:39:09 +0300

    removed session start

--- application/views/app_analytics/app.php
+++ application/views/app_analytics/app.php
@@ -3,8 +3,6 @@
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 
-session_start();
-
 <div class="content-wrapper">
 	<style>
 		#loading {
