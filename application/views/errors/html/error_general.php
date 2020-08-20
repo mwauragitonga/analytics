ca4d26e78c874cc553965930dd28ce9bbef2c3c8
Cyrus-Muchiri <cyrusmuchirichomba@gmail.com>
2020-08-21 00:40:25 +0300

    Error Messagees'

--- application/views/errors/html/error_general.php
+++ application/views/errors/html/error_general.php
@@ -1,64 +1,18 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="utf-8">
-<title>Error</title>
-<style type="text/css">
-
-::selection { background-color: #E13300; color: white; }
-::-moz-selection { background-color: #E13300; color: white; }
-
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
-
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
-
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
-
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
-
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	box-shadow: 0 0 8px #D0D0D0;
-}
-
-p {
-	margin: 12px 15px 12px 15px;
-}
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+	body { text-align: center; padding: 150px; }
+	h1 { font-size: 50px; }
+	body { font: 20px Helvetica, sans-serif; color: #333; }
+	article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+	a { color: #dc8100; text-decoration: none; }
+	a:hover { color: #333; text-decoration: none; }
 </style>
-</head>
-<body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
+
+<article>
+	<h1>We&rsquo;ll be back soon!</h1>
+	<div>
+		<p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:info@dawati.co.ke">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+		<p>&mdash; Dawati Team</p>
 	</div>
-</body>
-</html>
+</article>
